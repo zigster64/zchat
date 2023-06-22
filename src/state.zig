c46f68a40c684d05b534834e485e9722e9040519
𒀳 Scribe of the Ziggurat𒅆𒂍𒉪 <steve@raffmail.net>
2023-06-22 13:34:22 +1000

    use thread conditions and broadcasts to sync event steams to do realtime updates

--- src/state.zig
+++ src/state.zig
@@ -18,79 +18,86 @@ pub fn deinit(self: *Self) void {
     return self.document.deinit();
 }
 
-// because unicode - all things are a slice of u8 instead of simple bytes
-const ChatRequest = struct {
-    emoji: []u8,
-    byte: []u8,
-    char: []u8,
-    cuniform: []u8,
-};
-
-fn addBytes(self: *Self, bytes: []const u8) void {
+// addBytes will append new bytes to the document, then broadcast an update to all the event streams
+fn addBytes(self: *Self, bytes: []const u8) !void {
     self.document_mutex.lock();
     defer self.document_mutex.unlock();
-    self.document.appendSlice(bytes) catch |err| {
-        std.debug.print("Error appending to document - allocation error {any}\n", .{err});
-    };
+    try self.document.appendSlice(bytes);
     std.debug.print("The document is now '{s}'\n", .{self.document.items});
+
+    // signal the document as updated
+    {
+        self.event_mutex.lock();
+        defer self.event_mutex.unlock();
+        self.event_condition.broadcast();
+    }
 }
 
 // chat handler for receiving chat update, which has a JSON encoded object with the type of new thing to be added to the document
 pub fn chat(self: *Self, req: *httpz.Request, res: *httpz.Response) !void {
-    _ = self;
     if (try req.jsonObject()) |v| {
         if (v.get("cuniform")) |cuniform| {
-            std.debug.print("got cuniform 0x{s} {s}\n", .{ std.fmt.fmtSliceHexUpper(cuniform.string), cuniform.string });
-            self.addBytes(cuniform.string);
+            // std.debug.print("got cuniform 0x{s} {s}\n", .{ std.fmt.fmtSliceHexUpper(cuniform.string), cuniform.string });
+            try self.addBytes(cuniform.string);
         }
-        if (v.get("byte")) |byte| {
-            std.debug.print("got bytes 0x{s}\n", .{byte.string});
-        }
+        // if (v.get("byte")) |byte| {
+        //      std.debug.print("got bytes 0x{s}\n", .{byte.string});
+        // }
         if (v.get("char")) |char| {
-            std.debug.print("got char {s}\n", .{char.string});
-            self.addBytes(char.string);
+            // std.debug.print("got char {s}\n", .{char.string});
+            try self.addBytes(char.string);
         }
         if (v.get("emoji")) |emoji| {
-            std.debug.print("got emoji 0x{s} {s}\n", .{ std.fmt.fmtSliceHexUpper(emoji.string), emoji.string });
-            self.addBytes(emoji.string);
+            // std.debug.print("got emoji 0x{s} {s}\n", .{ std.fmt.fmtSliceHexUpper(emoji.string), emoji.string });
+            try self.addBytes(emoji.string);
         }
     }
-
-    // if (try req.jsonValue()) |v| {
-    //     std.debug.print("got value {any}\n", .{v});
-    // }
-
-    // if (try req.json(ChatRequest)) |chat_request| {
-    //     std.debug.print("Chat Request\n", .{});
-    //     std.debug.print(" emoji {s}\n", .{chat_request.emoji});
-    //     std.debug.print(" byte {s}\n", .{chat_request.byte});
-    //     std.debug.print(" char {s}\n", .{chat_request.char});
-    //     std.debug.print(" cuniform {s}\n", .{chat_request.cuniform});
-    // }
     res.body = "thanks";
+}
+
+pub fn writeDocument(self: *Self, res: *httpz.Response) !void {
+    self.document_mutex.lock();
+    defer self.document_mutex.unlock();
+
+    try res.stream.writeAll("event: document\n");
+    // split the document into lines
+    var lines = std.mem.tokenizeAny(u8, self.document.items, "\n");
+    while (lines.next()) |line| {
+        try res.stream.writeAll("data: ");
+        try res.stream.writeAll(line);
+        try res.stream.writeAll("\n\n");
+    }
 }
 
 pub fn events(self: *Self, req: *httpz.Request, res: *httpz.Response) !void {
     _ = req;
+
+    const keepalive_time = std.time.ns_per_s * 5;
+
+    // Set us up an event stream header, and send it to the client before doing anything
     res.useEventStream();
     try res.write();
+
+    // on initial connect, send them a copy of the document
+    try self.writeDocument(res);
+
+    // aquire a lock on the event_mutiex
+    self.event_mutex.lock();
+    defer self.event_mutex.unlock();
 
     // Loop forever, emitting the current document
     // loop will block on the Thread.Condition, which gets signalled on a Conditon.broadcast() when the document changes
     while (true) {
-        {
-            self.document_mutex.lock();
-            defer self.document_mutex.unlock();
-            // split the document into lines
-            var lines = std.mem.tokenizeAny(u8, self.document.items, "\n");
-            while (lines.next()) |line| {
-                std.debug.print("got line {s}\n", .{line});
-                try res.stream.writeAll("data: ");
-                try res.stream.writeAll(line);
-                try res.stream.writeAll("\n\n");
+        // note that condition.wait will unlock the event mutex, then wait for either a new event, or a timeout
+        // on return, this re-aquires the mutex
+        self.event_condition.timedWait(&self.event_mutex, keepalive_time) catch |err| {
+            if (err == error.Timeout) {
+                try res.stream.writeAll(": keep-alive ping\n\n");
+                continue;
             }
-        }
-
-        std.time.sleep(std.time.ns_per_s * 5);
+        };
+        //try if we get here, it means that the event_condition was signalled, so we can send the updated document now
+        std.debug.print("got an update signal !\n", .{});
+        try self.writeDocument(res);
     }
 }
